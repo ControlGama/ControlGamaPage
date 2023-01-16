1fb1544ea24a35573e33aea55e38da7d5b34d0a3
Gama Mac <nahalielgamaliel@hotmail.com>
2023-01-15 22:31:01 -0600

    Iteractive Courses

--- src/components/CoursesBox.jsx
+++ src/components/CoursesBox.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "@styles/componentsStyles/coursesBox.scss";
 import CoursesBoxItem from "@components/coursesBoxItem.jsx";
+import { DownCircleFilled } from "@ant-design/icons";
 
 const CoursesBox = ({ data }) => {
-  
-  const texts_CoursesBoxItem = data.texts_CoursesBoxItem; 
+  const texts_CoursesBoxItem = data.texts_CoursesBoxItem;
+  const [hiddenBox, setHiddenBox] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const el = ref.current;
+    el?.style.setProperty("display", hiddenBox ? "none" : "flex");
+  }, [hiddenBox]);
+
   return (
     <div className="CoursesBox">
-      <div className="CoursesTitle">{data.item.title}</div>
-      <div className="CoursesBody">
+      <div className="CoursesTitle" onClick={() => setHiddenBox(!hiddenBox)}>
+        <span>
+          <DownCircleFilled />
+        </span>
+        {data.item.title}
+      </div>
+      <div className="CoursesBody" ref={ref}>
         {data.item.coursesItems.map((item, index) => {
-          return <CoursesBoxItem key={`coursesItems${data.item.Key}${index}`} itemData={{item,texts_CoursesBoxItem}} />;
+          return (
+            <CoursesBoxItem
+              key={`coursesItems${data.item.Key}${index}`}
+              itemData={{ item, texts_CoursesBoxItem }}
+            />
+          );
         })}
       </div>
     </div>
